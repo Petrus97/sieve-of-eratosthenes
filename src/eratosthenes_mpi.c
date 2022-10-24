9cec05bbaccef9b343db14baf1145ff83e30fdbc
ale19 <ale.petru97@gmail.com>
2022-10-24 23:32:32 +0200

    send the primes to the master process

--- src/eratosthenes_mpi.c
+++ src/eratosthenes_mpi.c
@@ -77,6 +77,16 @@ void print_primes(const char *n_numbers, uint64_t max)
     }
 }
 
+void print_array(void *buf, int dim, int rank)
+{
+    int *tmp = (int *)buf;
+    for (size_t i = 0; i < dim; i++)
+    {
+        printf("[%ld]%d ", i + rank, tmp[i]);
+    }
+    printf("\n");
+}
+
 int main(int argc, char *argv[])
 {
     // Initialize MPI
@@ -85,7 +95,6 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int comm_size = 1;
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
-    printf("Comm size: %d\n", comm_size);
     uint64_t max = 100;
     uint64_t sqrt_max = (uint64_t)sqrt(max);
 
@@ -93,11 +102,11 @@ int main(int argc, char *argv[])
         0 (false) -> unmarked
         1 (true) -> marked
     */
-    char *natural_numbers = (char *)calloc(max + 1, sizeof(char));
+    char *natural_numbers = (char *)malloc((max + 1) * sizeof(char));
     memset(natural_numbers, false, max); // set all unmarked
 
     /*
-        1) The master process allocates the array of prime numbers and calculate the sqrt of them.
+        1) Each process allocates the array of prime numbers and calculate the sqrt of them.
     */
     uint64_t k = 2;
     while (square(k) <= sqrt_max)
@@ -114,72 +123,99 @@ int main(int argc, char *argv[])
     uint64_t end = (sqrt_max + 1) + BLOCK_HIGH(rank, comm_size, n);
     uint64_t blk_size = BLOCK_SIZE(rank, comm_size, n);
 
-    printf("[%d] low: %ld high: %ld size: %ld\n", rank, start, end, blk_size);
-    // Each process will build an array 
-    int *tmp_array = (int *)calloc(blk_size, sizeof(int)); // tmp array to send // BUG Does not work with char or uint8 arrays
-    // just to be sure
-    memset(tmp_array, false, blk_size);
-    /*
-        make the calculation with the for loops
-    */
-    for (uint64_t j = 2; j <= sqrt_max; j++)
-    {
-        if (!natural_numbers[j]) // unmarked
-        {
-            for (uint64_t i = start; i <= end; i++)
-            {
-                if (i % j == 0)
-                {
-                    natural_numbers[i] = true;
-                    tmp_array[i - start] = true;
-                }
-            }
-        }
-    }
+    // printf("[%d] low: %ld high: %ld size: %ld\n", rank, start, end, blk_size);
 
     // Send the tmp array to the master node (that will concatenate)
     if (rank != MASTER_NODE)
     {
-        // Send the array
-        if(MPI_Send(tmp_array, blk_size, MPI_INT, MASTER_NODE, COMM_TAG, MPI_COMM_WORLD) == MPI_SUCCESS)
+        int *tmp_array = (int *)malloc(blk_size * sizeof(int)); // tmp array to send // BUG Does not work with char or uint8 arrays
+        memset(tmp_array, false, blk_size);
+        if (tmp_array == NULL)
         {
-            printf("[%d] success\n", rank);
+            printf("Error allocate memory!");
         }
-        
-
+        else
+        {
+            print_array(tmp_array, blk_size, rank);
+        }
+        // Each process calculate its prime numbers
+        for (uint64_t j = 2; j <= sqrt_max; j++)
+        {
+            if (!natural_numbers[j]) // unmarked
+            {
+                for (uint64_t i = start; i <= end; i++)
+                {
+                    if (i % j == 0)
+                    {
+                        natural_numbers[i] = true;
+                        tmp_array[i - start] = true;
+                    }
+                }
+            }
+        }
+        // Send the array
+        if (MPI_Send(tmp_array, blk_size, MPI_INT, MASTER_NODE, COMM_TAG, MPI_COMM_WORLD) != MPI_SUCCESS)
+        {
+            printf("[%d] failed to send to Master Node\n", rank);
+        }
         free(tmp_array);
     }
     else // Master node
     {
-        int dim;
-        uint64_t start = sqrt_max + 1;
-        // for (int id = 1; id < comm_size; id++)
-        // {
-        MPI_Status status;
-        // Probe for an incoming message from slave process
-        MPI_Probe(1, COMM_TAG, MPI_COMM_WORLD, &status);
-
-        // When probe returns, the status object has the size and other
-        // attributes of the incoming message. Get the message size
-        MPI_Get_count(&status, MPI_INT, &dim);
-        printf("dim %d\n", dim);
-
-        // allocates the memory and receive the array
-        int *tmp = (int *)calloc(blk_size, sizeof(int));
-        // Receive the array in the allocated buffer
-        MPI_Recv(tmp, dim, MPI_INT, 1, COMM_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Received array of %d size:\n", dim);
-        for (size_t i = 0; i < dim; i++)
+        // Master process calculates its prima numbers
+        for (uint64_t j = 2; j <= sqrt_max; j++)
         {
-            printf("[%ld]%d ", i + start + BLOCK_LOW(1, comm_size, n), (int)tmp[i]);
+            if (!natural_numbers[j]) // unmarked
+            {
+                for (uint64_t i = start; i <= end; i++)
+                {
+                    if (i % j == 0)
+                    {
+                        natural_numbers[i] = true;
+                    }
+                }
+            }
         }
-        printf("\n");
-        free(tmp);
-        //}
+        // Retrieve the value calculated from the other processes
+        for (int id = 1; id < comm_size; id++)
+        {
+            // printf("[%d] receiving from %d...\n", rank, id);
+            uint64_t start_idx = (sqrt_max + 1) + BLOCK_LOW(id, comm_size, n);
+            int dim;
+            MPI_Status status;
+            // Probe for an incoming message from slave process
+            int ret = MPI_Probe(id, COMM_TAG, MPI_COMM_WORLD, &status);
+            if (ret != MPI_SUCCESS)
+                printf("Failed probing\n");
+            // When probe returns, the status object has the size and other
+            // attributes of the incoming message. Get the message size
+            ret = MPI_Get_count(&status, MPI_INT, &dim);
+            if (ret != MPI_SUCCESS)
+                printf("Failed getting count\n");
+            // printf("dim %d\n", dim);
+
+            // allocates the memory and receive the array
+            int *tmp = (int *)malloc(blk_size * sizeof(int));
+            if (tmp == NULL)
+            {
+                printf("Error allocate memory!");
+            }
+            // Receive the array in the allocated buffer
+            MPI_Recv(tmp, dim, MPI_INT, id, COMM_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            print_array(tmp, dim, start_idx);
+            for (size_t i = 0; i < dim; i++)
+            {
+                // printf("%ld ", start_idx + i);
+                natural_numbers[start_idx + i] = tmp[i];
+            }
+            // printf("\n");
+
+            free(tmp);
+        }
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    // if (rank == 0)
-    //     print_primes(natural_numbers, max);
+    if (rank == 0)
+        print_primes(natural_numbers, max);
 
     free(natural_numbers);
     MPI_Finalize();
